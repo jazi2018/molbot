adc8bb6b05a43ee517404dd767e59857a84977da
jazi2018 <jaredaziv@gmail.com>
2025-01-30 02:11:44 -0800

    added a command to check who the leader is

--- cog.py
+++ cog.py
@@ -11,6 +11,7 @@ class GroupLeader(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.sel_alpha.start()
+        self.group_leader = None
 
     def cog_unload(self):
         self.sel_alpha.cancel()
@@ -18,21 +19,28 @@ class GroupLeader(commands.Cog):
     @tasks.loop(time=midnight)
     async def sel_alpha(self):
         print("determining group leader.....")
-        guild_id = 1159064214476959794
+        guild_id = 1159064214476959794 #doncord
         guild = self.bot.get_guild(guild_id)
 
         if guild:
             members = [member for member in guild.members if not member.bot]
             if members:
-                group_leader = random.choice(members)
+                self.group_leader = random.choice(members)
                 channel = guild.get_channel(1159064215735246921)
                 if channel:
-                    await channel.send(f'rise and grind alphas.... todays group leader is {group_leader.mention}!!')
+                    await channel.send(f'rise and grind alphas.... todays group leader is {self.group_leader.mention}!!')
                     await channel.send(f'https://tenor.com/view/haechan-127hivemind-alpha-wolf-nct-nct-haechan-gif-26541424')
 
     @sel_alpha.before_loop
     async def before_sel_alpha(self):
         await self.bot.wait_until_ready()
+    
+    @commands.command()
+    async def leader(self, ctx):
+        if self.group_leader is None:
+            await ctx.send('erm. there is no group leader today. wtf???')
+        else:
+            await ctx.send(f'your sigma group leader for today is {self.group_leader.name}')
 
 async def setup(bot):
     await bot.add_cog(GroupLeader(bot))
